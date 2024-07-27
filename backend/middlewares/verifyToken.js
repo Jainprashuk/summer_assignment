d8303e11c5cdf9e601e3ab8f547cd87d51a10e49
prashuk jain <29jainprashuk@gmail.com>
2024-07-27 23:46:56 +0530

    ...

--- backend/middlewares/verifyToken.js
+++ backend/middlewares/verifyToken.js
@@ -60,39 +60,36 @@ const verifyToken = (req, res, next) => {
         const { authorization } = req.headers;
 
         if (!authorization) {
-            // return res.status(401).json({
-            //     status: "fail",
-            //    message: "Unauthorized: No authorization header",
-            //     data: {},
-            // });
-            next()
+            return res.status(401).json({
+                status: "fail",
+               message: "Unauthorized: No authorization header",
+                data: {},
+            });
         }
 
         const token = authorization.split(" ")[1];
 
         if (!token) {
-            // return res.status(401).json({
-            //     status: "fail",
-            //    message: "Unauthorized: No token provided",
-            //     data: {},
-            // });
-            next()
+            return res.status(401).json({
+                status: "fail",
+               message: "Unauthorized: No token provided",
+                data: {},
+            });
         }
 
         jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
-            // if (err) {
-            //     console.error("JWT verification error:", err);
-            //     return res.status(401).json({
-            //         status: "fail",
-            //        message: "Unauthorized: Invalid token",
-            //         data: {},
-            //     });
+            if (err) {
+                console.error("JWT verification error:", err);
+                return res.status(401).json({
+                    status: "fail",
+                   message: "Unauthorized: Invalid token",
+                    data: {},
+                });
 
-            // } else {
-            //     req.user = { email: decoded.data.email, _id: decoded.data._id };
-            //     next();
-            // }
-            next();
+            } else {
+                req.user = { email: decoded.data.email, _id: decoded.data._id };
+                next();
+            }
         });
     } catch (error) {
        console.error("Error in verifyToken middleware:", error);
